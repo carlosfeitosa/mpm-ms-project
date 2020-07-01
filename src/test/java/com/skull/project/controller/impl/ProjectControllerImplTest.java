3c630795bc26a8f6817ba2d11b4989c7e6ad149e
Carlos Feitosa <carlos.feitosa.nt@gmail.com>
2020-07-01 00:59:18 -0300

    cobertura do controller

---
+++ src/test/java/com/skull/project/controller/impl/ProjectControllerImplTest.java
@@ -0,0 +1,48 @@
+package com.skull.project.controller.impl;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import java.util.List;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.test.context.event.annotation.BeforeTestClass;
+
+import com.skull.project.controller.ProjectController;
+import com.skull.project.model.Project;
+
+@SpringBootTest
+class ProjectControllerImplTest {
+
+	@Autowired
+	private ProjectController controller;
+
+	@BeforeTestClass
+	void setupTest() {
+
+		((ProjectControllerImpl) controller).initMockDb();
+	}
+
+	@Test
+	void testGetAll() {
+		List<Project> projectList = controller.getAll();
+
+		assertThat(projectList.size()).isGreaterThan(0);
+	}
+
+	@Test
+	void testGetById() {
+		List<Project> projectList = controller.getAll();
+
+		for (Project project : projectList) {
+			ResponseEntity<Project> foundProject = controller.getById(project.getId());
+
+			assertThat(foundProject.getStatusCode()).isEqualTo(HttpStatus.OK);
+			assertThat(foundProject.getBody().getId()).isEqualTo(project.getId());
+		}
+	}
+
+}
