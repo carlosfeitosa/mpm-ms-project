469a69fb86019b722cc3f90818c0fdab3a107d1d
Carlos Feitosa <carlos.feitosa.nt@gmail.com>
2020-07-26 02:32:53 -0300

    Eureka autodiscovery

--- src/main/java/com/skull/project/MpmProjectServiceApplication.java
+++ src/main/java/com/skull/project/MpmProjectServiceApplication.java
@@ -2,6 +2,7 @@ package com.skull.project;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 /**
  * Microservice main class.
@@ -11,6 +12,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
  *
  */
 @SpringBootApplication
+@EnableDiscoveryClient
 public class MpmProjectServiceApplication {
 
 	public static void main(String[] args) {
